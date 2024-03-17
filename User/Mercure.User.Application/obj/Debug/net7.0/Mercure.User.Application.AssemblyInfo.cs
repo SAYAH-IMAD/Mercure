cfc0d093a7bfb6c891cecf788c5a33288a48c3bd
SAYAH Imad <i_sayah@esi.dz>
2024-03-17 18:01:07 -0400

    1- create project for identity server

--- User/Mercure.User.Application/obj/Debug/net7.0/Mercure.User.Application.AssemblyInfo.cs
+++ User/Mercure.User.Application/obj/Debug/net7.0/Mercure.User.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mercure.User.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd6c5aa38f8a4f8480802bdb92e3e8d207a7ca94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61cb57fb0f4ab14a8aee1210b7ebac4abaf63c55")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mercure.User.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mercure.User.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ User/Test/Mercure.User.Infrastructure.Test/UserRepositoryTest.cs
@@ -0,0 +1,145 @@
+using FluentAssertions;
+using Mercure.Common.Persistence.Transactions;
+using Mercure.Common.Persistence.Translator;
+using Mercure.User.Domain.Aggregate;
+using Mercure.User.Domain.ValueObject;
+using Mercure.User.Infrastructure.Persistence.Model;
+using Mercure.User.Infrastructure.Persistence.Repository;
+using Moq;
+using System.Collections;
+
+namespace Mercure.User.Infrastructure.Test
+{
+    public class UserRepositoryTest
+    {
+        readonly Mock<ITranslator<UserAggregate, UserModel>> _translatorMock;
+        readonly Mock<ITransaction<UserModel>>  _transactionMock;
+
+        public UserRepositoryTest()
+        {
+            _transactionMock = new Mock<ITransaction<UserModel>>();
+            _translatorMock = new Mock<ITranslator<UserAggregate, UserModel>>();
+        }
+
+        [Fact]
+        public void Add_Always_AddUser()
+        {
+            //Arrange
+            string firstName = "Imad";
+            string lastName = "SAYAH";
+            string email = "email";
+            string password = "password";
+            Address address = new("King Street", "California", "G1E 0M2");
+            DateTime birthDate = new(2024, 01, 01);
+
+            UserAggregate aggregate = UserAggregate.Create(firstName,
+                lastName,
+                email,
+                password,
+                address,
+                birthDate);
+
+            _translatorMock.Setup(e => e.Translate(It.IsAny<UserAggregate>()))
+                .Returns(It.IsAny<UserModel>());
+
+            _translatorMock.Setup(e => e.Translate(It.IsAny<UserModel>()))
+                .Returns(It.IsAny<UserAggregate>());
+
+            // Act 
+            new UserRepository(_translatorMock.Object, _transactionMock.Object)
+            .Add(ref aggregate);
+
+            // Assert
+
+            _transactionMock.Verify(e => e.Insert(It.IsAny<UserModel>()));
+            _translatorMock.Verify(e => e.Translate(It.IsAny<UserAggregate>()));
+            _translatorMock.Verify(e => e.Translate(It.IsAny<UserModel>()));
+        }
+
+        [Fact]
+        public void GetById_Always_ReturnUser()
+        {
+            //Arrange
+            int id = 1; 
+            string firstName = "Imad";
+            string lastName = "SAYAH";
+            string email = "email";
+            string password = "password";
+            Address address = new("King Street", "California", "G1E 0M2");
+            DateTime birthDate = new(2024, 01, 01);
+
+            UserAggregate aggregate = UserAggregate.Create(firstName,
+                lastName,
+                email,
+                password,
+                address,
+                birthDate);
+
+            _translatorMock.Setup(e => e.Translate(It.IsAny<UserAggregate>()))
+                .Returns(It.IsAny<UserModel>());
+
+            _translatorMock.Setup(e => e.Translate(It.IsAny<UserModel>()))
+                .Returns(It.IsAny<UserAggregate>());
+
+            // Act 
+           UserAggregate result = new UserRepository(_translatorMock.Object, _transactionMock.Object)
+            .GetById(id);
+
+            // Assert
+            result.Should().Be(aggregate);
+        }
+
+        [Fact]
+        public void Save_Always_SaveChanges()
+        {
+            //Arrange
+            int id = 1;
+            string firstName = "Imad";
+            string lastName = "SAYAH";
+            string email = "email";
+            string password = "password";
+            Address address = new("King Street", "California", "G1E 0M2");
+            DateTime birthDate = new(2024, 01, 01);
+
+            UserAggregate aggregate = new(id,
+                firstName,
+                lastName,
+                email,
+                password,
+                address,
+                birthDate,
+                [],
+                []);
+
+            UserModel model = new()
+            {
+                Id = id,
+                FirstName = firstName,
+                LastName = lastName,
+                Email = email,
+                Password = password,
+                BirthDate = birthDate,
+                City = "",
+                Street = "",
+                PostalCode = "12345",
+                Profiles = [],
+                HistoryStates = []
+            };
+
+            _transactionMock.Setup(e => e.GetByIdentifier(aggregate.Identifier.Value))
+                .Returns(model);
+
+            _translatorMock.Setup(e => e.Translate(aggregate))
+                .Returns(model);
+
+            _translatorMock.Setup(e => e.Translate(model))
+                .Returns(aggregate);
+
+            // Act 
+            new UserRepository(_translatorMock.Object, _transactionMock.Object)
+             .Save(ref aggregate);
+
+            // Assert
+        }
+    }
+}
