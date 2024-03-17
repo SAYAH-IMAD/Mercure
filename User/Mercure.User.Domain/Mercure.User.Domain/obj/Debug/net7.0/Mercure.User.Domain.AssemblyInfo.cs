26e4b000ecded777093cef6430135e445e472d5a
SAYAH Imad <i_sayah@esi.dz>
2024-03-17 18:04:00 -0400

    1 - add identity server to project Mercure.IdentityServer

--- User/Mercure.User.Domain/Mercure.User.Domain/obj/Debug/net7.0/Mercure.User.Domain.AssemblyInfo.cs
+++ User/Mercure.User.Domain/Mercure.User.Domain/obj/Debug/net7.0/Mercure.User.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mercure.User.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61cb57fb0f4ab14a8aee1210b7ebac4abaf63c55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfc0d093a7bfb6c891cecf788c5a33288a48c3bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mercure.User.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mercure.User.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
