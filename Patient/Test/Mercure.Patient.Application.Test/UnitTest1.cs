f29957728cba88d457b91d93e1b2d7026997a3e9
SAYAH Imad <i_sayah@esi.dz>
2024-07-30 18:43:28 -0400

    creating Test project

---
+++ Patient/Test/Mercure.Patient.Application.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Mercure.Patient.Application.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Patient/Test/Mercure.Patient.Infrastructure.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Mercure.Patient.Infrastructure.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
